490a43b44bff62eec052e6dc31c1cbe6df2c14d2
Anooshakhalid <144916298+Anooshakhalid@users.noreply.github.com>
2023-09-17 19:35:10 +0500

    task2.py

--- task2.py
+++ task2.py
@@ -3,7 +3,7 @@
 # I made a calculator by python object-oriented programming which is based on classes and objects
 
 # import abstract method to make a abstract class
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, ABC
 
 
 # classes for basic mathematical operation
@@ -14,60 +14,78 @@ class Operation(metaclass=ABCMeta):
         self.operand_2 = int(input('Enter your second operand:'))
 
     @abstractmethod
-    def matematical_operation(self):
+    def mathematical_operation(self):
         pass
 
 
 class Addition_operation(Operation):
 
-    def matematical_operation(self):
+    def mathematical_operation(self):
         self.input_operands()
         print('\nRESULT:\nAddition:', self.operand_1 + self.operand_2)
+        print()
 
 
-class Subtraction_operation(Operation):
+class Subtraction_operation(Operation, ABC):
 
-    def matematical_operation(self):
+    def mathematical_operation(self):
         self.input_operands()
         print('\nRESULT:\nSubtraction:', self.operand_1 - self.operand_2)
+        print()
 
 
 class Multiplication_operation(Operation):
 
-    def matematical_operation(self):
+    def mathematical_operation(self):
         self.input_operands()
         print('\nRESULT:\nMultiplication:', self.operand_1 * self.operand_2)
+        print()
 
 
 class Division_operation(Operation):
 
-    def matematical_operation(self):
+    def mathematical_operation(self):
         self.input_operands()
-        print('\nRESULT:\nDivision:', self.operand_1 / self.operand_2)
+        print('\nRESULT:\nDivision:', round((self.operand_1 / self.operand_2),4))
+        print()
 
 
 # main interface
 print('**** HELLO PYTHON CALCULATOR ****')
 print()
-print("Here's your basic operation:")
-print('PRESS[1] FOR ADDITION \nPRESS[2] FOR SUBTRACTION \nPRESS[3] FOR MULTIPLICATION \nPRESS[4] FOR DIVISION\n')
-choice = int(input('Which operation you want to perform:'))
 
-if choice == 1:
-    a1 = Addition_operation()
-    a1.matematical_operation()
+try:
+    while True:
+        print("Here's your basic operation:")
+        print('PRESS[1] FOR ADDITION \nPRESS[2] FOR SUBTRACTION \nPRESS[3] FOR MULTIPLICATION \nPRESS[4] FOR DIVISION')
+        print('PRESS[5] FOR EXIT\n')
+        choice = int(input('Which operation do you want to perform:'))
 
-elif choice == 2:
-    s1 = Subtraction_operation()
-    s1.matematical_operation()
+        if choice == 1:
+            a1 = Addition_operation()
+            a1.mathematical_operation()
 
-elif choice == 3:
-    m1 = Multiplication_operation()
-    m1.matematical_operation()
+        elif choice == 2:
+            s1 = Subtraction_operation()
+            s1.mathematical_operation()
 
-elif choice == 4:
-    d1 = Division_operation()
-    d1.matematical_operation()
+        elif choice == 3:
+            m1 = Multiplication_operation()
+            m1.mathematical_operation()
 
-else:
-    print('Invalid Choice!!\nPLS choose a given numbers')
+        elif choice == 4:
+            d1 = Division_operation()
+            d1.mathematical_operation()
+
+        elif choice == 5:
+            print("Thankyou for using.\nGoodbye!")
+            exit()
+
+        else:
+            print('Invalid Choice!!\nPLS choose a given numbers')
+
+except ValueError:
+    print("Invalid Input!! Enter an integer")
+
+
+
